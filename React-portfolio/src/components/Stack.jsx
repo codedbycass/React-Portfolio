d64838d866e2afa78cf2d289bd5adfe4d3fe6f32
cassmano <cassandramanotham@gmail.com>
2024-02-27 18:11:31 -0500

    fix white space

--- React-portfolio/src/components/Stack.jsx
+++ React-portfolio/src/components/Stack.jsx
@@ -1,7 +1,7 @@
 export default function Stack() {
     return(
-        <div className="bg-green-400 font-work font-light uppercase">
-            <h4 className="gliding-animation whitespace-nowrap pl-15">Open to: full-time roles, freelance projects, and coffee chats!</h4>
+        <div className="bg-green-400 font-work font-light uppercase animate-pulse">
+            <h4 className="text-center text-white">Open to: full-time roles, freelance projects, and coffee chats!</h4>
         </div>
     )
 }
