7e1d65c538fbf4f9f84ec02b21a2db2e6477581c
cassmano <cassandramanotham@gmail.com>
2024-03-05 15:02:53 -0500

    edited IP project

--- React-portfolio/src/components/IP.jsx
+++ React-portfolio/src/components/IP.jsx
@@ -10,7 +10,6 @@ import ip9 from '../assets/IP/ip9.png'
 import ip10 from '../assets/IP/ip10.png'
 import ip11 from '../assets/IP/ip11.png'
 import ip12 from '../assets/IP/ip12.png'
-import ip13 from '../assets/IP/ip13.png'
 import ip14 from '../assets/IP/ip14.png'
 import ip15 from '../assets/IP/ip15.png'
 import ip16 from '../assets/IP/ip16.png'
@@ -35,7 +34,6 @@ export default function IP() {
             <img src={ip10} className='py-0.5' />
             <img src={ip11} className='py-0.5' />
             <img src={ip12} className='py-0.5' />
-            <img src={ip13} className='py-0.5' />
             <img src={ip14} className='py-0.5' />
             <img src={ip15} className='py-0.5' />
             <img src={ip16} className='py-0.5' />
